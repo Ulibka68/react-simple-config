8e2fa7b6cb6e2c71c511e1bc2d24df12e0aedb18
Ulibka68 <gena68@inbox.ru>
2021-12-11 13:29:48 +0300

    без modules компилируется, вывод пустой

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import style from './index.module.css';
+// import style from './index.module.css';
+import style from './index.css';
 
 ReactDOM.render(
   <div>
-    <h1 className={style.h1Background}>React boilerplate</h1>
+    <div>
+      <h1 className={style.h1Background}>React boilerplate</h1>
+    </div>
+    <div>
+      <h1>Просто React boilerplate</h1>
+    </div>
   </div>,
   document.getElementById('root')
 );
